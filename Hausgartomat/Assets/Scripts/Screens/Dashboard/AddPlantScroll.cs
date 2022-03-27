0b555bc8fd2480eb4963303b26d913caa162e59a
Steven Diestel <82208488+Izamii@users.noreply.github.com>
2022-03-27 01:04:07 +0100

    Add Plant Screens updated by Azure Prototype and added some custom sprites

---
+++ Hausgartomat/Assets/Scripts/Screens/Dashboard/AddPlantScroll.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddPlantScroll : MonoBehaviour
+{
+    [SerializeField] private Text plantNameBox;
+    [SerializeField] private Button nextPlantButton;
+    [SerializeField] private Button previousPlantButton;
+    private List<Plant> plantList;
+    private GetPlantData database;
+    private int currentPlant;
+    private void Start()
+    {
+        database = GameObject.Find("Firebase").GetComponent<GetPlantData>();
+        StartCoroutine(WaitForDatabase());
+    }
+
+    public void RightButtonClick()
+    {
+        if (plantList.Count == 0 || (plantList.Count-1) < (currentPlant+1))
+        {
+            return;
+        }
+        
+        currentPlant += 1;
+        plantNameBox.text = plantList[currentPlant].name;
+        UpdateButtons(); 
+    }
+    
+    public void LeftButtonClick()
+    {
+        if (plantList.Count == 0 || 0 > currentPlant-1 )
+        {
+            return;
+        }
+        
+        currentPlant -= 1;
+        plantNameBox.text = plantList[currentPlant].name;
+        UpdateButtons();
+    }
+    
+    private IEnumerator WaitForDatabase()
+    {
+        yield return new WaitUntil(() => database.read);
+        plantList = database.GETAllPlants();
+        if (plantList.Count != 0)
+        {
+            plantNameBox.text = plantList[currentPlant].name;
+            UpdateButtons();
+        }
+        else
+        {
+            previousPlantButton.interactable = false;
+            nextPlantButton.interactable = false;
+        }
+    }
+
+    public Plant GetSelectedPlant()
+    {
+        return plantList[currentPlant];
+    }
+
+    private void UpdateButtons()
+    {
+        previousPlantButton.interactable = currentPlant != 0;
+        nextPlantButton.interactable = currentPlant != plantList.Count-1;
+    }
+}
